a46280512ed31302da874400a25211b7cf0e8ed3
Khalid El amrani <kel-amra@e2r8p13.1337.ma>
2022-02-18 15:58:41 +0100

    fixing errors

--- error_msg.c
+++ error_msg.c
@@ -6,7 +6,7 @@
 /*   By: kel-amra <kel-amra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/17 23:35:58 by kel-amra          #+#    #+#             */
-/*   Updated: 2022/02/17 23:54:44 by kel-amra         ###   ########.fr       */
+/*   Updated: 2022/02/18 15:26:24 by kel-amra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	error_msg(t_pipex *tmp)
 		ft_printf("Error: Command not found: %s\n", tmp->command1[0]);
 	else if (tmp->status[0] == 1)
 		ft_printf("Error: Permission denied: %s\n", tmp->command1[0]);
-	else if (tmp->status[1] == 2)
+	if (tmp->status[1] == 2)
 		ft_printf("Error: command not found: %s\n", tmp->command2[0]);
 	else if (tmp->status[1] == 1)
 		ft_printf("Error: Permission denied: %s\n", tmp->command2[0]);
